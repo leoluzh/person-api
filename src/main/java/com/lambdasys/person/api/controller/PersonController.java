928923e1921fc7271c4b0ae391f4e9b37c15dd10
Leonardo Fernandes <leonardo.l.fernandes@gmail.com>
2021-05-04 02:00:14 -0300

    Inclusao de metodo para criacao de person

--- src/main/java/com/lambdasys/person/api/controller/PersonController.java
+++ src/main/java/com/lambdasys/person/api/controller/PersonController.java
@@ -1,19 +1,35 @@
 package com.lambdasys.person.api.controller;
 
+import com.lambdasys.person.api.dto.PersonDto;
+import com.lambdasys.person.api.entity.Person;
+import com.lambdasys.person.api.mapper.PersonMapper;
 import com.lambdasys.person.api.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/v1/persons")
 public class PersonController {
 
     private final PersonRepository repository;
+    private final PersonMapper personMapper = PersonMapper.INSTANCE;
 
     @Autowired
     public PersonController(PersonRepository repository){
         this.repository = repository;
+    }
+
+    @PostMapping()
+    public ResponseEntity<PersonDto> create(@RequestBody @Valid PersonDto personDto){
+        Person person = personMapper.toEntity(personDto);
+        person = repository.save(person);
+        return ResponseEntity.ok(personMapper.toDto(person));
     }
 
 }
