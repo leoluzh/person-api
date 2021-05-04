9761ec8e9658ffeec4578b4f23513ed2889ce6f4
Leonardo Fernandes <leonardo.l.fernandes@gmail.com>
2021-05-04 01:53:40 -0300

    Inclusao de injecao de dependencia do repository de person.

--- src/main/java/com/lambdasys/person/api/controller/PersonController.java
+++ src/main/java/com/lambdasys/person/api/controller/PersonController.java
@@ -1,9 +1,19 @@
 package com.lambdasys.person.api.controller;
 
+import com.lambdasys.person.api.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/persons")
 public class PersonController {
+
+    private final PersonRepository repository;
+
+    @Autowired
+    public PersonController(PersonRepository repository){
+        this.repository = repository;
+    }
+
 }
