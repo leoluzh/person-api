707f6da444e7fc9004c29366c16a18d629f8b40c
Leonardo Fernandes <leonardo.l.fernandes@gmail.com>
2021-05-04 00:56:56 -0300

    Mapeamento de propriedade para jpa.

--- src/main/java/com/lambdasys/person/api/entity/Phone.java
+++ src/main/java/com/lambdasys/person/api/entity/Phone.java
@@ -2,19 +2,32 @@ package com.lambdasys.person.api.entity;
 
 import com.lambdasys.person.api.enums.PhoneType;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.io.Serializable;
 
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
+
+@Entity
+@Table(name="phone")
 public class Phone implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
     private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name="type")
     private PhoneType type;
+
+    @Column(name="number",length=20)
     private String number;
 
 }
