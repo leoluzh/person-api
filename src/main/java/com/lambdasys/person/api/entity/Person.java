3bb171ab5b548193c747cc077401dae0b4a1956e
Leonardo Fernandes <leonardo.l.fernandes@gmail.com>
2021-05-04 00:29:10 -0300

    Inclusao de propriedades para modelo.

--- src/main/java/com/lambdasys/person/api/entity/Person.java
+++ src/main/java/com/lambdasys/person/api/entity/Person.java
@@ -1,8 +1,23 @@
 package com.lambdasys.person.api.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.io.Serializable;
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 
 @SuppressWarnings("serial")
 public class Person implements Serializable {
+
+    private Long id;
+    private String name;
+    private String surname;
+    private String cpf;
+    private LocalDate birthdate;
 
 }
