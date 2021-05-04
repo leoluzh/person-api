69e63b66efa8784613d42f398bd9211fb826f9a9
Leonardo Fernandes <leonardo.l.fernandes@gmail.com>
2021-05-04 01:45:05 -0300

    Inclusao de recursos spring data - jpa repository

--- src/main/java/com/lambdasys/person/api/repository/PersonRepository.java
+++ src/main/java/com/lambdasys/person/api/repository/PersonRepository.java
@@ -1,4 +1,9 @@
 package com.lambdasys.person.api.repository;
 
-public interface PersonRepository {
+import com.lambdasys.person.api.entity.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PersonRepository extends JpaRepository<Person,Long> {
 }
