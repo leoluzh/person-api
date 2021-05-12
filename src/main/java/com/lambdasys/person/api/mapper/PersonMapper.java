dd64315ec68e4e0957749de593c05b88933676cc
Leonardo Fernandes <leonardo.l.fernandes@gmail.com>
2021-05-12 00:30:36 -0300

    Implementacao de mapper para entidade person.

--- src/main/java/com/lambdasys/person/api/mapper/PersonMapper.java
+++ src/main/java/com/lambdasys/person/api/mapper/PersonMapper.java
@@ -2,10 +2,13 @@ package com.lambdasys.person.api.mapper;
 
 import com.lambdasys.person.api.dto.PersonDto;
 import com.lambdasys.person.api.entity.Person;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
 
+@Mapper
 public interface PersonMapper {
 
-    PersonMapper INSTANCE = null;
+    PersonMapper INSTANCE = Mappers.getMapper(PersonMapper.class);
 
     Person toEntity(PersonDto personDto);
     PersonDto toDto(Person person);
