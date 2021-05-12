c12dd228903c62d9bfdb590b3f09b1b4d1b59ab2
Leonardo Fernandes <leonardo.l.fernandes@gmail.com>
2021-05-11 23:58:00 -0300

    Tabela de relacionamento entre pessoa e telefone.

--- src/main/resources/db/migration/V1_1_2__create_person_phone_table.sql
+++ src/main/resources/db/migration/V1_1_2__create_person_phone_table.sql
@@ -1,5 +1,5 @@
 
-CREATE TABLE IF NOT EXISTS person_phone (
+CREATE TABLE IF NOT EXISTS person_phones (
     person_id INTEGER NOT NULL ,
     phone_id INTEGER NOT NULL ,
     UNIQUE(person_id,phone_id)
