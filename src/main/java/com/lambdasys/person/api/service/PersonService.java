55f508faf5d4ce2ad22ddeb3aad7339f2564acfb
Leonardo Fernandes <leonardo.l.fernandes@gmail.com>
2021-05-11 23:57:32 -0300

    Metodo para criar entidade.

--- src/main/java/com/lambdasys/person/api/service/PersonService.java
+++ src/main/java/com/lambdasys/person/api/service/PersonService.java
@@ -1,4 +1,27 @@
 package com.lambdasys.person.api.service;
 
+import com.lambdasys.person.api.dto.PersonDto;
+import com.lambdasys.person.api.entity.Person;
+import com.lambdasys.person.api.mapper.PersonMapper;
+import com.lambdasys.person.api.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class PersonService {
+
+    private final PersonRepository personRepository;
+    private final PersonMapper personMapper = PersonMapper.INSTANCE;
+
+    @Autowired
+    public PersonService(PersonRepository personRepository){
+        this.personRepository = personRepository;
+    }
+
+    public PersonDto create(PersonDto personDto){
+        Person person = personMapper.toEntity(personDto);
+        person = personRepository.save(person);
+        return personMapper.toDto(person);
+    }
+
 }
