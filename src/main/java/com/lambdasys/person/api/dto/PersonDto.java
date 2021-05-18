eae05dd81ca83b66d839b61d5a4603045c297891
Leonardo Fernandes <leonardo.l.fernandes@gmail.com>
2021-05-17 23:46:16 -0300

    Incluindo mapeamento e validacao de campos.

--- src/main/java/com/lambdasys/person/api/dto/PersonDto.java
+++ src/main/java/com/lambdasys/person/api/dto/PersonDto.java
@@ -1,7 +1,44 @@
 package com.lambdasys.person.api.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.br.CPF;
+
+import javax.validation.Valid;
+import javax.validation.constraints.*;
 import java.io.Serializable;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 
 @SuppressWarnings("serial")
 public class PersonDto implements Serializable {
+
+    private Long id;
+
+    @NotBlank
+    @Size(min=2,max=50)
+    private String name;
+
+    @NotBlank
+    @Size(min=1,max=255)
+    private String surname;
+
+    @NotBlank
+    @CPF
+    private String cpf;
+
+    @NotNull
+    @PastOrPresent
+    private String birthdate;
+
+    @Valid
+    @NotEmpty
+    private List<PhoneDto> phones;
+
 }
