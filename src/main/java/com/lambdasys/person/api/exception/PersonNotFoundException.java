86dc860aea668d52f0b325697ba86d182af7f9d6
Leonardo Fernandes <leonardo.l.fernandes@gmail.com>
2021-05-18 01:56:45 -0300

    Inclusao de exception para pessoa no encontrada na pesquisa por id.

--- src/main/java/com/lambdasys/person/api/exception/PersonNotFoundException.java
+++ src/main/java/com/lambdasys/person/api/exception/PersonNotFoundException.java
@@ -1,2 +1,13 @@
-package com.lambdasys.person.api;public class PersonNotFoundException {
+package com.lambdasys.person.api.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class PersonNotFoundException extends Exception {
+
+    public PersonNotFoundException(Long id){
+        super(String.format("Person with id %s not found.",id));
+    }
+
 }
